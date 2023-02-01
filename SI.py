c15cc285fe5ae8dd6c3062ffe1d01388fb5d5389
Abhisheks648 <Abhisheks648@gmail.com>
2023-02-01 12:18:08 +0530

    Add files via upload

---
+++ SI.py
@@ -0,0 +1,9 @@
+def simple_interest(principal, rate, time):
+  return (principal * rate * time) / 100
+
+p = 10000
+r = 10
+t = 2
+
+interest = simple_interest(p, r, t)
+print("Simple Interest: ", interest)
